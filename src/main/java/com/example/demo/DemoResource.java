778b2ac39e6a31a10db6df63801d21a00443b68c
Arinze G Ojiaku <ojiakuarinze@gmail.com>
2020-07-10 01:10:13 +0100

    Added REST interface

---
+++ src/main/java/com/example/demo/DemoResource.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author BLADE
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoResource {
+    
+        @RequestMapping(path = "/test", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<String> subscribe() {
+        return ResponseEntity.ok("Testing Azure DevOps");
+    }
+}
